c6937ed230b4c1e63fdf9dfafd558f1340b6306d
5933software <jmrobotics@jmchs.org>
2026-01-20 17:07:37 -0700

    got a start on the shooter subsystem

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.shooter;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ShooterIO {
+    
+    @AutoLog
+    public static class ShooterIOInputs {
+
+        public double shooterCurrentAmps = 0.0;
+        public double shooterAppliedVolts = 0.0;
+        public double shooterVelocityRadPerSec = 0.0;
+
+    }
+
+    public default void updateInputs(ShooterIOInputs inputs) {}
+}
