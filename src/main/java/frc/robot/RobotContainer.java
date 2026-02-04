5a6c0659020663fe7b9c8f9e502bb7348edf4fb6
5933software <jmrobotics@jmchs.org>
2026-02-03 17:20:58 -0700

    now it actually runs the selected path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -219,7 +219,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return Commands.none();
+    return autoChooser.get();
   }
 
   public void resetSimulationField() {
