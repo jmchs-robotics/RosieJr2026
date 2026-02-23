35256f890f0e749a09c8b690768830437d85a0b3
Nixon Draper <nd29059@jmchs.org>
2026-02-23 15:28:17 -0700

    Made IntakeUp for Intake to go up

---
+++ src/main/java/frc/robot/commands/IntakeUp.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.intake.Intake;
+
+public class IntakeUp extends Command {
+
+  private final Intake m_IntakeUp;
+
+  public IntakeUp(Intake IntakeUp) {
+
+    m_IntakeUp = IntakeUp;
+    addRequirements(m_IntakeUp);
+  }
+
+  @Override
+  public void initialize() {}
+
+  @Override
+  public void execute() {
+    m_IntakeUp.setSlapDownMotor(-0.15);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_IntakeUp.setSlapDownMotor(0);
+  }
+}
