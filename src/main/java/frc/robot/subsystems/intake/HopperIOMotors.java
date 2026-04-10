a34a9fa78781457ed0d5dc04e028b867279b281c
5933software <jmrobotics@jmchs.org>
2026-04-10 11:00:11 -0600

    changed the neo we burned to a talon

--- src/main/java/frc/robot/subsystems/intake/HopperIOMotors.java
+++ src/main/java/frc/robot/subsystems/intake/HopperIOMotors.java
@@ -1,24 +1,22 @@
 package frc.robot.subsystems.intake;
 
-import static frc.robot.util.SparkUtil.*;
+//import static frc.robot.util.SparkUtil.*;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
-import java.util.function.DoubleSupplier;
+ import com.ctre.phoenix6.hardware.TalonFX;
+// import com.revrobotics.RelativeEncoder;
+// import com.revrobotics.spark.SparkLowLevel.MotorType;
+// import com.revrobotics.spark.SparkMax;
+// import java.util.function.DoubleSupplier;
 
 public class HopperIOMotors implements HopperIO {
 
-  private final SparkMax hopperMotor;
-  private final RelativeEncoder hopperEncoder;
+  private final TalonFX hopperMotor;
 
   private final TalonFX shootIndexMotor;
 
   public HopperIOMotors() {
 
-    hopperMotor = new SparkMax(12, MotorType.kBrushless);
-    hopperEncoder = hopperMotor.getEncoder();
+    hopperMotor = new TalonFX(12);
 
     shootIndexMotor = new TalonFX(15);
   }
@@ -31,18 +29,11 @@ public class HopperIOMotors implements HopperIO {
     inputs.shootIndexVelocityRotPerSec = shootIndexMotor.getRotorVelocity().getValueAsDouble();
     inputs.shootIndexIsConnected = shootIndexMotor.isConnected();
 
-    sparkStickyFault = false;
 
-    ifOk(
-        hopperMotor,
-        hopperEncoder::getVelocity,
-        (value) -> inputs.hopperVelocityRotPerSec = value * 60);
-    ifOk(
-        hopperMotor,
-        new DoubleSupplier[] {hopperMotor::getAppliedOutput, hopperMotor::getBusVoltage},
-        (values) -> inputs.hopperAppliedVolts = values[0] * values[1]);
-    ifOk(hopperMotor, hopperMotor::getOutputCurrent, (value) -> inputs.hopperCurrentAmps = value);
-    inputs.hopperIsConnected = !sparkStickyFault;
+    inputs.hopperAppliedVolts = shootIndexMotor.getSupplyVoltage().getValueAsDouble();
+    inputs.hopperCurrentAmps = shootIndexMotor.getSupplyCurrent().getValueAsDouble();
+    inputs.hopperVelocityRotPerSec = shootIndexMotor.getRotorVelocity().getValueAsDouble();
+    inputs.hopperIsConnected = shootIndexMotor.isConnected();
   }
 
   @Override
